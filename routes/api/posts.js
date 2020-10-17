54dc17a7c518d49097da3af42975913bd6dc2df4
Reece Kenney <reecekenney50@gmail.com>
2020-10-17 17:41:09 +0100

    retweetSchemaUpdate

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -69,4 +69,37 @@ router.put("/:id/like", async (req, res, next) => {
     res.status(200).send(post)
 })
 
+router.post("/:id/retweet", async (req, res, next) => {
+    var postId = req.params.id;
+    var userId = req.session.user._id;
+
+    // Try and delete retweet
+    var deletedPost = await Post.findOneAndDelete({ postedBy: userId, retweetData: postId })
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+    var option = deletedPost != null ? "$pull" : "$addToSet";
+
+    return res.status(200).send(option);
+
+    // Insert user like
+    req.session.user = await User.findByIdAndUpdate(userId, { [option]: { likes: postId } }, { new: true})
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+    // Insert post like
+    var post = await Post.findByIdAndUpdate(postId, { [option]: { likes: userId } }, { new: true})
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+
+    res.status(200).send(post)
+})
+
 module.exports = router;

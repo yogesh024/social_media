6cfb6562b2cfce756dcdf06f18b7fa693030fd46
Reece Kenney <reecekenney50@gmail.com>
2020-10-17 10:43:37 +0100

    postIdFromElement

--- public/js/common.js
+++ public/js/common.js
@@ -31,9 +31,21 @@ $("#submitPostButton").click(() => {
     })
 })
 
-$(document).on("click", ".likeButton", () => {
-    alert("button clicked");
+$(document).on("click", ".likeButton", (event) => {
+    var button = $(event.target);
+    var postId = getPostIdFromElement(button);
+    console.log(postId);
 })
+
+function getPostIdFromElement(element) {
+    var isRoot = element.hasClass("post");
+    var rootElement = isRoot == true ? element : element.closest(".post");
+    var postId = rootElement.data().id;
+
+    if(postId === undefined) return alert("Post id undefined");
+
+    return postId;
+}
 
 function createPostHtml(postData) {
     
@@ -46,7 +58,7 @@ function createPostHtml(postData) {
     var displayName = postedBy.firstName + " " + postedBy.lastName;
     var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
 
-    return `<div class='post'>
+    return `<div class='post' data-id='${postData._id}'>
 
                 <div class='mainContentContainer'>
                     <div class='userImageContainer'>

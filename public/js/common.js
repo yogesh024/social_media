920255dc713d47ae14403ec57445123c94c907b6
Reece Kenney <reecekenney50@gmail.com>
2020-10-17 15:16:36 +0100

    updateLikesNumber

--- public/js/common.js
+++ public/js/common.js
@@ -41,7 +41,9 @@ $(document).on("click", ".likeButton", (event) => {
         url: `/api/posts/${postId}/like`,
         type: "PUT",
         success: (postData) => {
-            console.log(postData.likes.length);
+            
+            button.find("span").text(postData.likes.length || "");
+
         }
     })
 
@@ -97,6 +99,7 @@ function createPostHtml(postData) {
                             <div class='postButtonContainer'>
                                 <button class='likeButton'>
                                     <i class='far fa-heart'></i>
+                                    <span>${postData.likes.length || ""}</span>
                                 </button>
                             </div>
                         </div>

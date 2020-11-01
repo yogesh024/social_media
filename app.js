44961242759df69a51f775e1fc533905808ca644
Reece Kenney <reecekenney50@gmail.com>
2020-11-01 22:10:47 +0000

    serveImage

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
 const postRoute = require('./routes/postRoutes');
 const profileRoute = require('./routes/profileRoutes');
+const uploadRoute = require('./routes/uploadRoutes');
 
 // Api routes
 const postsApiRoute = require('./routes/api/posts');
@@ -35,6 +36,7 @@ app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 app.use("/posts", middleware.requireLogin, postRoute);
 app.use("/profile", middleware.requireLogin, profileRoute);
+app.use("/uploads", uploadRoute);
 
 app.use("/api/posts", postsApiRoute);
 app.use("/api/users", usersApiRoute);

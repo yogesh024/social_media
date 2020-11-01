1cdf1b64650b9dee18d31dd11b1485de22443323
Reece Kenney <reecekenney50@gmail.com>
2020-11-01 13:45:56 +0000

    followingData

---
+++ public/js/followersAndFollowing.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+
+    if(selectedTab === "followers") {
+        loadFollowers();
+    }
+    else {
+        loadFollowing();
+    }
+});
+
+function loadFollowers() {
+    $.get(`/api/users/${profileUserId}/followers`, results => {
+        outputUsers(results, $(".resultsContainer"));
+    })
+}
+
+function loadFollowing() {
+    $.get(`/api/users/${profileUserId}/following`, results => {
+        outputUsers(results, $(".resultsContainer"));
+    })
+}
+
+function outputUsers(data, container) {
+    console.log(data);
+}

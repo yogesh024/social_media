2155ebfd145adf859e69e2e5bcefdaed65dfd402
Reece Kenney <reecekenney50@gmail.com>
2020-11-03 21:54:40 +0000

    chatNameAjax

---
+++ public/js/chatPage.js
@@ -0,0 +1,17 @@
+$("#chatNameButton").click(() => {
+    var name = $("#chatNameTextbox").val().trim();
+    
+    $.ajax({
+        url: "/api/chat/" + chatId,
+        type: "PUT",
+        data: { chatName: name },
+        success: (data, status, xhr) => {
+            if(xhr.status != 204) {
+                alert("could not update");
+            }
+            else {
+                location.reload();
+            }
+        }
+    })
+})

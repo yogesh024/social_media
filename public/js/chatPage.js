b9ab279b81751f11c6848acc3d63731976e44ca1
Reece Kenney <reecekenney50@gmail.com>
2020-11-03 22:00:17 +0000

    chatNameRoute

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -2,7 +2,7 @@ $("#chatNameButton").click(() => {
     var name = $("#chatNameTextbox").val().trim();
     
     $.ajax({
-        url: "/api/chat/" + chatId,
+        url: "/api/chats/" + chatId,
         type: "PUT",
         data: { chatName: name },
         success: (data, status, xhr) => {

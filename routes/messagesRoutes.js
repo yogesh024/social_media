5302100f2a38096dfaf9240160e3fcd538523b28
Reece Kenney <reecekenney50@gmail.com>
2020-11-03 15:57:38 +0000

    getChatForPage

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const bodyParser = require("body-parser")
 const bcrypt = require("bcrypt");
 const User = require('../schemas/UserSchema');
+const Chat = require('../schemas/ChatSchema');
 
 router.get("/", (req, res, next) => {
     res.status(200).render("inboxPage", {
@@ -21,11 +22,23 @@ router.get("/new", (req, res, next) => {
     });
 })
 
-router.get("/:chatId", (req, res, next) => {
+router.get("/:chatId", async (req, res, next) => {
+
+    var userId = req.session.user._id;
+    var chatId = req.params.chatId;
+
+    var chat = await Chat.findOne({ _id: chatId, users: { $elemMatch: { $eq: userId } } })
+    .populate("users");
+
+    if(chat == null) {
+        // Check if chat id is really user id
+    }
+
     res.status(200).render("chatPage", {
         pageTitle: "Chat",
         userLoggedIn: req.session.user,
-        userLoggedInJs: JSON.stringify(req.session.user)
+        userLoggedInJs: JSON.stringify(req.session.user),
+        chat: chat
     });
 })
 

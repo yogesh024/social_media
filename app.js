66e22efa0253fb4a2bad1710dd9771b3aaf5c1de
Reece Kenney <reecekenney50@gmail.com>
2020-11-03 10:03:03 +0000

    chatRoutes

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const messagesRoute = require('./routes/messagesRoutes');
 // Api routes
 const postsApiRoute = require('./routes/api/posts');
 const usersApiRoute = require('./routes/api/users');
+const chatsApiRoute = require('./routes/api/chats');
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
@@ -44,6 +45,7 @@ app.use("/messages", middleware.requireLogin, messagesRoute);
 
 app.use("/api/posts", postsApiRoute);
 app.use("/api/users", usersApiRoute);
+app.use("/api/chats", chatsApiRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
--- public/js/common.js
+++ public/js/common.js
@@ -259,6 +259,17 @@ $("#userSearchTextbox").keydown((event) => {
 
 })
 
+$("#createChatButton").click(() => {
+    var data = JSON.stringify(selectedUsers);
+
+    $.post("/api/chats", { users: data }, chat => {
+
+        if(!chat || !chat._id) return alert("Invalid response from server.");
+
+        window.location.href = `/messages/${chat._id}`;
+    })
+})
+
 $(document).on("click", ".likeButton", (event) => {
     var button = $(event.target);
     var postId = getPostIdFromElement(button);

3f5cfd6a9a92ff8b95c1f6544dfcce3327b5a71f
Reece Kenney <reecekenney50@gmail.com>
2020-11-04 09:18:45 +0000

    createMessage

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const messagesRoute = require('./routes/messagesRoutes');
 const postsApiRoute = require('./routes/api/posts');
 const usersApiRoute = require('./routes/api/users');
 const chatsApiRoute = require('./routes/api/chats');
+const messagesApiRoute = require('./routes/api/messages');
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
@@ -46,6 +47,7 @@ app.use("/messages", middleware.requireLogin, messagesRoute);
 app.use("/api/posts", postsApiRoute);
 app.use("/api/users", usersApiRoute);
 app.use("/api/chats", chatsApiRoute);
+app.use("/api/messages", messagesApiRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
--- schemas/MessageSchema.js
+++ schemas/MessageSchema.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const chatSchema = new Schema({
+const messageSchema = new Schema({
     sender: { type: Schema.Types.ObjectId, ref: 'User' },
     content: { type: String, trim: true },
     chat: { type: Schema.Types.ObjectId, ref: 'Chat' },
     readBy: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 }, { timestamps: true });
 
-module.exports = mongoose.model('Chat', chatSchema);
+module.exports = mongoose.model('Message', messageSchema);

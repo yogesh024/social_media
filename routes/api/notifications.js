61b551ca5a56fb83e85aefad9f18cf542e23bcd0
Reece Kenney <reecekenney50@gmail.com>
2020-11-05 13:22:16 +0000

    createNotificationHtml

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -11,6 +11,16 @@ const Notification = require('../../schemas/NotificationSchema');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", async (req, res, next) => {
+    
+    Notification.find({ userTo: req.session.user._id, notificationType: { $ne: "newMessage" } })
+    .populate("userTo")
+    .populate("userFrom")
+    .sort({ createdAt: -1 })
+    .then(results => res.status(200).send(results))
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
 
 })
 
